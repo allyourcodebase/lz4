3742d792fe100f52ce3d66dc0ea7fe8fce92bde4
Techatrix <techatrix@mailbox.org>
2024-08-15 13:55:18 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+[![CI](https://github.com/allyourcodebase/lz4/actions/workflows/ci.yaml/badge.svg)](https://github.com/allyourcodebase/lz4/actions)
+
+# lz4
+
+This is [lz4](https://github.com/lz4/lz4), packaged for [Zig](https://ziglang.org/).
+
+## Installation
+
+First, update your `build.zig.zon`:
+
+```
+# Initialize a `zig build` project if you haven't already
+zig init
+zig fetch --save git+https://github.com/allyourcodebase/lz4.git#1.10.0
+```
+
+You can then import `lz4` in your `build.zig` with:
+
+```zig
+const lz4_dependency = b.dependency("lz4", .{
+    .target = target,
+    .optimize = optimize,
+});
+your_exe.linkLibrary(lz4_dependency.artifact("lz4"));
+```
---
+++ build.zig
@@ -0,0 +1,34 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const upstream = b.dependency("lz4", .{});
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const strip = b.option(bool, "strip", "Omit debug information");
+    const pic = b.option(bool, "pie", "Produce Position Independent Code");
+
+    const lz4 = b.addStaticLibrary(.{
+        .name = "lz4",
+        .target = target,
+        .optimize = optimize,
+        .pic = pic,
+        .strip = strip,
+        .link_libc = true,
+    });
+    b.installArtifact(lz4);
+    lz4.addIncludePath(upstream.path("lib"));
+    lz4.installHeader(upstream.path("lib/lz4.h"), "lz4.h");
+    lz4.installHeader(upstream.path("lib/lz4hc.h"), "lz4hc.h");
+    lz4.installHeader(upstream.path("lib/lz4frame.h"), "lz4frame.h");
+    lz4.addCSourceFiles(.{
+        .root = upstream.path("lib"),
+        .files = &.{
+            "lz4.c",
+            "lz4file.c",
+            "lz4frame.c",
+            "lz4hc.c",
+            "xxhash.c",
+        },
+    });
+}
